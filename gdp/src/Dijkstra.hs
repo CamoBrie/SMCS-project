a8a7ef4ecb3ec28b3cf7c2c48eff18f30c046686
Camobrie <bvmeggelen@gmail.com>
2024-03-18 15:50:14 +0100

    add sized

--- gdp/src/Dijkstra.hs
+++ gdp/src/Dijkstra.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
 module Dijkstra where
 
 -- https://mmhaskell.com/blog/2022/8/22/dijkstras-algorithm-in-haskell
 
-import Data.Maybe (fromMaybe)
-import MinHeap as H (MinHeap, extractMin, fromList, insert)
+import Debug.Trace (traceShow)
+import GHC.TypeLits (type (+))
+import MinHeap as H
 
 type HashMap k v = [(k, v)]
 
@@ -22,56 +28,59 @@ addDist :: (Num a) => Distance a -> Distance a -> Distance a
 addDist (Dist x) (Dist y) = Dist (x + y)
 addDist _ _ = Infinity
 
+neighbors :: String -> Graph -> [(String, Int)]
+neighbors node g = case lookup node (edges g) of
+  Just x -> x
+  Nothing -> []
+
 (!??) :: (Eq k) => HashMap k (Distance d) -> k -> Distance d
-(!??) m key = fromMaybe Infinity (lookup key m)
+(!??) m key = case (lookup key m) of
+  Just x -> x
+  Nothing -> Infinity
 
 newtype Graph = Graph
   {edges :: HashMap String [(String, Int)]}
 
-data DijkstraState = DijkstraState
+data DijkstraState n = DijkstraState
   { visitedSet :: HashSet String,
     distanceMap :: HashMap String (Distance Int),
-    nodeQueue :: MinHeap (Distance Int) String
+    nodeQueue :: H.Sized n (MinHeap (Distance Int) String)
   }
   deriving (Show)
 
-findShortestDistance :: Graph -> String -> String -> Distance Int
-findShortestDistance graph src dest = processQueue initialState !?? dest
+-- | Run Dijkstra's algorithm on a graph, starting from a given node.
+initialState :: String -> Graph -> (forall n. Graph -> DijkstraState n -> r) -> r
+initialState start g f = H.fromList [(Dist 0, start)] $ \h -> f g $ DijkstraState [] [(start, Dist 0)] h
+
+-- | Run Dijkstra's algorithm on a graph, starting from a given node.
+-- The result is a map of distances from the start node to all other nodes.
+dijkstra :: String -> String -> Graph -> Distance Int
+dijkstra start end graph = (initialState start graph $ \g s -> go g s) !?? end
   where
-    initialVisited = []
-    initialDistances = [(src, (Dist 0))]
-    initialQueue = H.fromList [(Dist 0, src)]
-    initialState = DijkstraState initialVisited initialDistances initialQueue
+    go :: Graph -> DijkstraState n -> HashMap String (Distance Int)
+    go g s
+      | H.isEmpty (nodeQueue s) = distanceMap s
+      | otherwise = go g $ step g s
 
-    processQueue :: DijkstraState -> HashMap String (Distance Int)
-    processQueue ds@(DijkstraState v0 d0 q0) = case H.extractMin q0 of
-      Nothing -> d0
-      Just ((_, node), q1) ->
-        if node == dest
-          then d0
-          else
-            if node `elem` v0
-              then processQueue (ds {nodeQueue = q1})
-              else -- Update the visited set
+-- | Perform one step of Dijkstra's algorithm.
+step :: Graph -> DijkstraState n -> DijkstraState n
+step g s = traceShow s $ case H.extractMin (nodeQueue s) of
+  Just ((Dist _, node), q) -> foldr (checkNeighbor node) (DijkstraState (node : visitedSet s) (distanceMap s) q) (neighbors node g)
 
-                let v1 = node : v0
-                    -- Get all unvisited neighbors of our current node
-                    allNeighbors = fromMaybe [] (lookup node (edges graph))
-                    unvisitedNeighbors = filter (\(n, _) -> not (n `elem` v1)) allNeighbors
-                 in -- Fold each neighbor and recursively process the queue
-                    processQueue $ foldl (foldNeighbor node) (DijkstraState v1 d0 q1) unvisitedNeighbors
+-- | Update the state based on a neighbor of the current node.
+checkNeighbor :: String -> (String, Int) -> DijkstraState n -> DijkstraState n
+checkNeighbor node (neighbor, weight) s
+  | neighbor `elem` visitedSet s = s
+  | otherwise =
+      let newDist = addDist (Dist weight) (distanceMap s !?? node)
+       in case newDist < (distanceMap s !?? neighbor) of
+            True -> DijkstraState (visitedSet s) ((neighbor, newDist) : distanceMap s) (H.insert (newDist, neighbor) (nodeQueue s))
+            False -> s
 
-    foldNeighbor :: String -> DijkstraState -> (String, Int) -> DijkstraState
-    foldNeighbor current ds@(DijkstraState v1 d0 q1) (neighborNode, cost) =
-      let altDistance = addDist (d0 !?? current) (Dist cost)
-       in if altDistance < d0 !?? neighborNode
-            then DijkstraState v1 ((neighborNode, altDistance) : d0) (H.insert (altDistance, neighborNode) q1)
-            else ds
-
--- Example graph
-graph1 :: Graph
-graph1 =
-  Graph $
+-- example graph
+exampleGraph :: Graph
+exampleGraph =
+  Graph
     [ ("A", [("D", 100), ("B", 1), ("C", 20)]),
       ("B", [("D", 50)]),
       ("C", [("D", 20)]),
--- gdp/src/MinHeap.hs
+++ gdp/src/MinHeap.hs
@@ -1,37 +1,88 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+
 module MinHeap
   ( MinHeap,
+    Sized,
     fromList,
     insert,
     merge,
     extractMin,
+    isEmpty,
   )
 where
 
--- https://github.com/nytr0gen/hs-min-heap/blob/master/MinHeap.hs
+import GHC.TypeLits
 
-import Prelude hiding (head, length, null, tail)
+--- GDP Stuff
+data Sized (n :: Nat) a = Sized a
 
+--- MinHeap Stuff
 data MinHeap k a
   = Leaf
   | Node a (MinHeap k a) (MinHeap k a) k
 
 instance (Show a, Show k) => Show (MinHeap k a) where
   show Leaf = "Leaf"
-  show (Node a _ _ k) = "Node " ++ show a ++ " " ++ show k
+  show (Node a l r k) = concat ["Node ", show a, " ", show k, " (", show l, ") (", show r, ")"]
 
-fromList :: (Ord k) => [(k, a)] -> MinHeap k a
-fromList = foldr insert Leaf
+instance (Show (MinHeap k a)) => Show (Sized n (MinHeap k a)) where
+  show (Sized h) = show h
 
-insert :: (Ord k) => (k, a) -> MinHeap k a -> MinHeap k a
-insert (k, a) h = merge h (Node a Leaf Leaf k)
+-- | Create a new heap from a list of key-value pairs.
+-- GDP invariants:
+-- 1. The keys in the heap are sorted
+-- 2. The size of the heap is equal to the length of the list
+-- Create a new heap with a known size.
+fromList :: forall k a r. (Ord k) => [(k, a)] -> (forall n. Sized n (MinHeap k a) -> r) -> r
+fromList xs k = k (Sized (fromList' xs) :: Sized n (MinHeap k a))
 
-merge :: (Ord k) => MinHeap k a -> MinHeap k a -> MinHeap k a
-merge h Leaf = h
-merge Leaf h = h
-merge h1@(Node a1 l1 r1 k1) h2@(Node a2 l2 r2 k2)
-  | k1 <= k2 = Node a1 (merge h2 r1) l1 k1
-  | otherwise = Node a2 (merge h1 r2) l2 k2
+fromList' :: (Ord k) => [(k, a)] -> MinHeap k a
+fromList' = foldr insert' Leaf
 
-extractMin :: (Ord k) => MinHeap k a -> Maybe ((k, a), MinHeap k a)
-extractMin Leaf = Nothing
-extractMin (Node a l r k) = Just ((k, a), merge l r)
+-- | Insert a new key-value pair into the heap.
+-- GDP invariants:
+-- 1. The keys in the heap are sorted
+-- 2. The size of the heap is increased by 1
+insert :: (Ord k) => (k, a) -> Sized n (MinHeap k a) -> Sized (n) (MinHeap k a)
+insert (k, a) (Sized h) = Sized (insert' (k, a) h)
+
+insert' :: (Ord k) => (k, a) -> MinHeap k a -> MinHeap k a
+insert' (k, a) h = merge' h (Node a Leaf Leaf k)
+
+-- | Merge two heaps into one.
+-- GDP invariants:
+-- 1. The keys in the heap are sorted
+-- 2. The size of the heap is increased by the sum of the sizes of the input heaps
+merge :: (Ord k) => Sized n (MinHeap k a) -> Sized m (MinHeap k a) -> Sized (n + m) (MinHeap k a)
+merge (Sized h1') (Sized h2') = Sized (merge' h1' h2')
+
+merge' :: (Ord k) => MinHeap k a -> MinHeap k a -> MinHeap k a
+merge' h Leaf = h
+merge' Leaf h = h
+merge' h1@(Node a1 l1 r1 k1) h2@(Node a2 l2 r2 k2)
+  | k1 <= k2 = Node a1 (merge' h2 r1) l1 k1
+  | otherwise = Node a2 (merge' h1 r2) l2 k2
+
+-- | Extract the minimum key-value pair from the heap.
+-- GDP invariants:
+-- 1. The keys in the heap are sorted
+-- 2. The size of the heap is reduced by 1
+extractMin :: (Ord k) => Sized (n) (MinHeap k a) -> Maybe ((k, a), Sized n (MinHeap k a))
+extractMin (Sized h) = case extractMin' h of
+  Nothing -> Nothing
+  Just (x, h') -> Just (x, Sized h')
+
+extractMin' :: (Ord k) => MinHeap k a -> Maybe ((k, a), MinHeap k a)
+extractMin' Leaf = Nothing
+extractMin' (Node a l r k) = Just ((k, a), merge' l r)
+
+isEmpty :: Sized n (MinHeap k a) -> Bool
+isEmpty (Sized Leaf) = True
+isEmpty _ = False
