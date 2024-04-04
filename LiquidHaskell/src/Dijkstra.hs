364c7cb37521d1e317d2ff7cfd7801419136af92
markg2000 <mrk3600@gmail.com>
2024-04-04 21:40:53 +0200

    tmp

--- LiquidHaskell/src/Dijkstra.hs
+++ LiquidHaskell/src/Dijkstra.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-@ LIQUID "--exact-data-con" @-}
-{-@ LIQUID "--no-totality" @-}
-{-@ LIQUID "--no-termination" @-}
+-- {-@ LIQUID "--no-totality" @-}
+-- {-@ LIQUID "--no-termination" @-}
 -- {-@ LIQUID "--totality" @-}
 
 module Dijkstra where
@@ -10,46 +10,70 @@ module Dijkstra where
 
 import Data.Maybe (fromMaybe)
 import MinHeap as H (MinHeap, extractMin, fromList, insert)
--- import qualified Data.HashMap.Strict as HM
+import qualified Data.HashMap.Strict as HM
 
-{-@ type Nat = {v:Int | v >= 0} @-}
+-- {-@ type Nat = {v:Int | v >= 0} @-}
 
 type HashMap k v = [(k, v)]
 
 type HashSet a = [a]
 
-data Distance a = Dist a | Infinity
-  deriving (Show, Eq)
+-- {-@ data Distance a = Dist a | Infinity @-}
+data Distance = Dist Int | Infinity
+  deriving (Show, Ord, Eq)
 
-instance (Ord a) => Ord (Distance a) where
-  Infinity <= Infinity = True
-  Infinity <= Dist _   = False
-  Dist _   <= Infinity = True
-  Dist x   <= Dist y   = x <= y
+-- instance (Ord a) => Ord (Distance a) where
+--   Infinity <= Infinity = True
+--   Infinity <= Dist _   = False
+--   Dist _   <= Infinity = True
+--   Dist x   <= Dist y   = x <= y
 
--- {-@ type validDistance = Distance<Nat> @-}
+-- instance Eq a => Eq (Distance a) where
+--   (Dist x) == (Dist y) = x == y
+--   Infinity == Infinity = True
+--   _ == _ = False
 
--- {-@ addDist :: (Num a) => 
---     x:ValidDistance 
---     -> y:ValidDistance 
---     -> {z:ValidDistance | z >= x + y} 
---     @-}
-addDist :: (Num a) => Distance a -> Distance a -> Distance a
+
+-- {-@ measure isValidDistance :: ValidDistance -> Bool @-}
+{-@ measure isValidDistance @-}
+isValidDistance :: Distance -> Bool
+isValidDistance (Dist v) = v >= 0
+isValidDistance Infinity = True
+
+-- {-@ measure compareValidDistances :: (Num a, Ord a) =>  ValidDistance a -> ValidDistance a -> Bool @-}
+-- {-@ measure compareValidDistances @-}
+-- compareValidDistances :: Distance -> Distance -> Bool
+-- compareValidDistances (Dist v1) (Dist v2) = v1 >= v2
+-- compareValidDistances Infinity  _         = True
+-- compareValidDistances _         _         = False
+
+{-@ type ValidDistance = {v:Distance | isValidDistance v} @-}
+
+-- {-@ type ValidDistance a = Distance {v:a | v >= 0 } @-}
+-- {-@ type VMinHeap k a X = { v: MinHeap k a | isMinHeap v X } @-}
+
+-- {-@ addDist :: (Num a, Ord a) 
+--             => x:ValidDistance a
+--             -> y:ValidDistance a
+--             -> {z:ValidDistance a | compareValidDistances z x && compareValidDistances z y} 
+--   @-}
+{-@ addDist :: ValidDistance -> ValidDistance -> ValidDistance @-}
+addDist :: Distance -> Distance -> Distance
 addDist (Dist x) (Dist y) = Dist (x + y)
 addDist _ _ = Infinity
 
--- {-@ (!??) :: (Eq k) => HashMap k (Distance d) -> k -> ValidDistance @-}
+{-@ (!??) :: (Eq k) => HashMap k ValidDistance -> k -> ValidDistance @-}
 -- {-@ (!??) :: (Eq k) => 
 --     m:HashMap k ValidDistance 
 --     -> k:k 
 --     -> {v:ValidDistance | k `HM.member` m} 
 --     @-}
-(!??) :: (Eq k) => HashMap k (Distance d) -> k -> Distance d
+(!??) :: (Eq k) => HashMap k Distance -> k -> Distance
 (!??) m key = fromMaybe Infinity (lookup key m)
 
--- {-@ data Graph = Graph
---       { edges :: HashMap String [(String, validDistance)] }
---   @-}
+{-@ data Graph = Graph
+      { edges :: HashMap String [(String, {x:Int | x >= 0})] }
+  @-}
 newtype Graph = Graph
   {edges :: HashMap String [(String, Int)]}
 
@@ -62,10 +86,16 @@ newtype Graph = Graph
 --     , nodeQueue   :: MinHeap ValidDistance (NodeInGraph g)
 --     }
 --   @-}
+{-@ data DijkstraState = DijkstraState
+    { visitedSet  :: HashSet String
+    , distanceMap :: HashMap String ValidDistance
+    , nodeQueue   :: MinHeap ValidDistance String
+    }
+  @-}
 data DijkstraState = DijkstraState
   { visitedSet :: HashSet String,
-    distanceMap :: HashMap String (Distance Int),
-    nodeQueue :: MinHeap (Distance Int) String
+    distanceMap :: HashMap String Distance,
+    nodeQueue :: MinHeap Distance String
   }
   deriving (Show)
 
@@ -75,15 +105,22 @@ data DijkstraState = DijkstraState
 --    -> (NodeInGraph g)
 --    -> validDistance 
 --    @-}
-findShortestDistance :: Graph -> String -> String -> Distance Int
+{-@ findShortestDistance :: Graph
+                         -> String
+                         -> String
+                         -> ValidDistance
+   @-}
+findShortestDistance :: Graph -> String -> String -> Distance
 findShortestDistance graph src dest = processQueue initialState !?? dest
   where
     initialVisited = []
-    initialDistances = [(src, (Dist 0))]
+    initialDistances = [(src, Dist 0)]
     initialQueue = H.fromList [(Dist 0, src)]
     initialState = DijkstraState initialVisited initialDistances initialQueue
 
-    processQueue :: DijkstraState -> HashMap String (Distance Int)
+    {-@ lazy processQueue @-}
+    {-@ processQueue :: (Eq k) => DijkstraState -> HashMap String ValidDistance @-}
+    processQueue :: DijkstraState -> HashMap String Distance
     processQueue ds@(DijkstraState v0 d0 q0) = case H.extractMin q0 of
       Nothing -> d0
       Just ((_, node), q1) ->
@@ -97,10 +134,11 @@ findShortestDistance graph src dest = processQueue initialState !?? dest
                 let v1 = node : v0
                     -- Get all unvisited neighbors of our current node
                     allNeighbors = fromMaybe [] (lookup node (edges graph))
-                    unvisitedNeighbors = filter (\(n, _) -> not (n `elem` v1)) allNeighbors
+                    unvisitedNeighbors = filter (\(n, _) -> n `notElem` v1) allNeighbors
                  in -- Fold each neighbor and recursively process the queue
                     processQueue $ foldl (foldNeighbor node) (DijkstraState v1 d0 q1) unvisitedNeighbors
 
+    {-@ foldNeighbor :: String -> DijkstraState -> (String, {x:Int | x >= 0}) -> DijkstraState @-}
     foldNeighbor :: String -> DijkstraState -> (String, Int) -> DijkstraState
     foldNeighbor current ds@(DijkstraState v1 d0 q1) (neighborNode, cost) =
       let altDistance = addDist (d0 !?? current) (Dist cost)
@@ -111,9 +149,57 @@ findShortestDistance graph src dest = processQueue initialState !?? dest
 -- Example graph
 graph1 :: Graph
 graph1 =
-  Graph $
+  Graph
     [ ("A", [("D", 100), ("B", 1), ("C", 20)]),
       ("B", [("D", 50)]),
       ("C", [("D", 20)]),
       ("D", [])
     ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+-- module Blank where
+
+-- {-@ type IsValidDist = {v: Int | v >= 0 } @-}
+
+-- {-@ addDist :: x:IsValidDist -> y:IsValidDist -> {z:IsValidDist | z == x + y } @-}
+-- addDist :: Int -> Int -> Int
+-- addDist x y = x + y
+
+
+-- data Distance = Dist Int | Infinity
+
+-- {-@ measure isValid :: Distance -> Bool @-}
+-- isValid :: Distance -> Bool 
+-- isValid (Dist d) = d >= 0
+-- isValid Infinity = True
+
+-- {-@ type IsValidDist = {v: Distance | isValid v } @-}
+
+-- {-@ measure compareValidDistances :: Distance  -> Distance  -> Bool @-}
+-- compareValidDistances ::  Distance  -> Distance  -> Bool
+-- compareValidDistances (Dist v1) (Dist v2) = v1 >= v2
+-- compareValidDistances Infinity  _         = True
+-- compareValidDistances _         Infinity  = True
+-- compareValidDistances _         _         = False
+
+-- {-@ predicate IsEqual Z X Y = compareValidDistances Z X && compareValidDistances Z Y @-}
+
+-- {-@ addDist :: x:IsValidDist -> y:IsValidDist -> {z:IsValidDist | IsEqual z x y } @-}
+-- addDist :: Distance -> Distance -> Distance
+-- addDist (Dist x) (Dist y) = Dist (x + y)
+-- addDist _       _       = Infinity
+
+-- {-@ invariant {d:Distance | isValid d <=> (d == Infinity || distanceValue d >= 0)} @-}
+
--- LiquidHaskell/src/MinHeap.hs
+++ LiquidHaskell/src/MinHeap.hs
@@ -1,6 +1,6 @@
-{-@ LIQUID "--exact-data-con" @-}
-{-@ LIQUID "--no-totality" @-}
-{-@ LIQUID "--no-termination" @-}
+-- {-@ LIQUID "--exact-data-con" @-}
+-- {-@ LIQUID "--no-totality" @-}
+-- {-@ LIQUID "--no-termination" @-}
 
 module MinHeap
   ( MinHeap,
@@ -13,16 +13,29 @@ where
 
 -- https://github.com/nytr0gen/hs-min-heap/blob/master/MinHeap.hs
 
-import Prelude hiding (head, length, null, tail)
+import Prelude hiding (head, length, null, tail,max)
+-- import Language.Haskell.Liquid.Prelude
 
 {-@ data MinHeap k a   = Leaf
-                  | Node { root  :: a
-                          , val   :: k
-                         , left  :: VMinHeap k a val
-                         , right :: VMinHeap k a val 
+                        | Node { root  :: a
+                                , val   :: k
+                              , left  :: MinHeap k a  
+                              , right :: MinHeap k a   
                           } @-}
 
-{-@ type VMinHeap k a X = MinHeap {v:k | X <= v} a        @-}
+-- {-@ type VMinHeap k a X = MinHeap {v:k | X <= v} a        @-}
+
+{-@ type VMinHeap k a X = { v: MinHeap k a | isMinHeap v X } @-}
+
+-- {-@ inline isMinHeap @-}
+{-@ isMinHeap :: (Ord k) => MinHeap k a -> k -> Bool @-}
+isMinHeap :: Ord k => MinHeap k a -> k -> Bool
+isMinHeap heap key = case heap of
+  Leaf           -> True
+  (Node _ k l r) -> key <= k -- && isMinHeap l k && isMinHeap r k
+
+isMinHeap Leaf _ = True
+isMinHeap (Node _ k l r) x = x >= k && isMinHeap l k && isMinHeap r k
 
 {-@ type NonEmptyMinHeap k a = {v:MinHeap k a | not (isEmpty v)} @-}
 
@@ -32,32 +45,155 @@ isEmpty Leaf = True
 isEmpty _    = False
 
 data MinHeap k a = Leaf
-           | Node { root  :: a
-                  , val   :: k
-                  , left  :: MinHeap k a
-                  , right :: MinHeap k a
+                  | Node { root  :: a
+                         , val   :: k
+                         , left  :: MinHeap k a
+                         , right :: MinHeap k a
                   }
+              deriving(Ord,Eq)
+
+instance Foldable (MinHeap k) where
+  foldMap _ Leaf = mempty
+  foldMap f (Node root _ left right) =
+      foldMap f left `mappend`
+      f root `mappend`
+      foldMap f right
+
+-- instance (Ord k, Ord a) => Ord (MinHeap k a) where
+--     Leaf <= _ = True  -- Leaf is smaller than any Node
+--     _ <= Leaf = False -- Any Node is greater than Leaf
+--     (Node k1 _ _ _) <= (Node k2 _ _ _) = k1 <= k2
+
+-- -- Define a custom Eq instance for MinHeap
+-- instance (Eq k, Eq a) => Eq (MinHeap k a) where
+--     Leaf == Leaf = True
+--     (Node k1 v1 _ _) == (Node k2 v2 _ _) = k1 == k2 && v1 == v2
+--     _ == _ = False  -- Leaf and Node are not equal
 
 
 instance (Show a, Show k) => Show (MinHeap k a) where
   show Leaf = "Leaf"
   show (Node a _ _ k) = "Node " ++ show a ++ " " ++ show k
 
-fromList :: (Ord k) => [(k, a)] -> MinHeap k a
+-- {-@ predicate AllValuesPresent H =
+--     AllPresent (toList H)
+-- @-}
+
+-- {-@ predicate AllPresent X =
+--       foldr (\x acc -> x `elem` (allValues acc)) true X
+--   @-}
+
+
+
+-- {-@ predicate LengthMatch :: [(k, a)] -> MinHeap k a -> Bool @-}
+{-@ predicate LengthMatch X H =
+      listLength X == heapLength H && isEqual X H
+    @-}
+
+{-@ isCorrect :: (Ord k, Ord a) => [(k, a)] -> MinHeap k a -> Bool @-}
+isCorrect :: (Ord k, Ord a) => [(k, a)] -> MinHeap k a -> Bool
+isCorrect x h = listLength x == heapLength h && isEqual x h
+
+-- {-@ measure heapLength @-}
+-- heapLength :: MinHeap k a -> Int
+-- heapLength Leaf           = 0
+-- heapLength (Node _ _ l r) = 1 + heapLength l + heapLength r
+
+{-@ measure listLength @-}
+listLength :: [(k, a)] -> Int
+listLength []     = 0
+listLength (_:xs) = 1 + listLength xs
+
+toList :: MinHeap k a -> [(k, a)]
+toList Leaf = []
+toList (Node a k l r) = toList l ++ [(k, a)] ++ toList r
+
+isEqual :: (Eq k, Eq a) => [(k,a)] -> MinHeap k a -> Bool
+isEqual xs heap = foldr (\x acc -> x `elem` xs && acc) True (toList heap)
+
+areElements :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a -> Bool
+areElements h1 h2 = foldr (\x acc -> x `elem` h2 && acc) True h1
+
+-- {-@ fromList :: (Ord k,Ord a) 
+--              => xs:[(k, a)] 
+--              -> {v:MinHeap k a | isCorrect xs v} 
+--   @-} 
+fromList :: (Ord k, Ord a) => [(k, a)] -> MinHeap k a
 fromList = foldr insert Leaf
 
-{-@ insert :: (Ord k) => (k, a) -> MinHeap k a -> MinHeap k a @-}
-insert :: (Ord k) => (k, a) -> MinHeap k a -> MinHeap k a
+-- {-@ insert :: (Ord k, Ord a) 
+--            => (k, a) 
+--            -> v1:MinHeap k a 
+--            -> {v: NonEmptyMinHeap k a | (heapLength v == ((heapLength v1) + 1))} 
+--   @-}
+insert :: (Ord k, Ord a) => (k, a) -> MinHeap k a -> MinHeap k a
 insert (k, a) h = merge h (Node a k Leaf Leaf)
 
-{-@ merge ::Ord k => MinHeap k a -> MinHeap k a -> MinHeap k a  @-}
-merge :: (Ord k) => MinHeap k a -> MinHeap k a -> MinHeap k a
+-- {-@ merge :: (Ord k, Ord a) 
+--           => h1:MinHeap k a 
+--           -> h2:MinHeap k a 
+--           -> {v:MinHeap k a | heapLength v == (heapLength h1 + heapLength h2 - 1) 
+--                             && areElements h1 v 
+--                             && areElements h2 v}  
+--   @-}
+{-@ lazy merge @-}
+{-@ merge :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a -> MinHeap k a @-}
+merge :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a -> MinHeap k a
 merge h Leaf = h
 merge Leaf h = h
 merge h1@(Node a1 k1 l1 r1 ) h2@(Node a2 k2 l2 r2 )
   | k1 <= k2 = Node a1 k1 (merge h2 r1) l1 
   | otherwise = Node a2 k2 (merge h1 r2) l2
 
-extractMin :: (Ord k) => MinHeap k a -> Maybe ((k, a), MinHeap k a)
+-- {-@ areElements :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a -> Bool  @-}
+-- areElements :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a -> Bool
+-- areElements Leaf Leaf = True
+-- areElements Leaf _ = False
+-- areElements (Node k v l r) h2 = l == h2 || r == h2 --|| areElements l h2 || areElements r h2
+
+-- {-@ predicate Lee H G = ((heapLength H) < (heapLength G)) @-}
+
+-- {-@ isSmaller :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a -> Bool@-}
+-- isSmaller :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a -> Bool
+-- isSmaller x y = heapLength x < heapLength y
+
+{-@ lazy heapLength @-}
+{-@ measure heapLength @-}
+heapLength :: MinHeap k a -> Int
+heapLength Leaf           = 0
+heapLength (Node _ _ l r) = 1 + nodeHeight l r
+
+{-@ lazy nodeHeight @-}
+{-@ inline nodeHeight @-}
+nodeHeight l r = 1 + max hl hr
+  where
+    hl         = heapLength l
+    hr         = heapLength r
+    
+{-@ inline max @-}
+max :: Int -> Int -> Int
+max x y = if x > y then x else y
+
+{-@ outputSmaller :: (Ord k, Ord a) => xs:NonEmptyMinHeap k a -> {ys:MinHeap k a | heapLength ys < heapLength xs} @-}
+outputSmaller :: (Ord k, Ord a) => MinHeap k a -> MinHeap k a
+outputSmaller Leaf = Leaf 
+outputSmaller (Node _ _ l r) = r
+
+-- {-@ measure ll @-}
+-- ll :: [a] -> Int
+-- ll []     = 0
+-- ll (_:xs) = 1 + ll xs
+
+-- {-@ outSmaller :: {xs:[a] | ll xs > 0} -> {ys:[a] | ll ys < ll xs} @-}
+-- outSmaller :: [a] -> [a]
+-- outSmaller [] = []
+-- outSmaller (_:xs) = xs
+
+
+-- {-@ extractMin :: (Ord k, Ord a) 
+--                => h:NonEmptyMinHeap k a 
+--                -> Maybe ((k,a),{v: MinHeap k a | heapLength v < heapLength h}) 
+--   @-}
+extractMin :: (Ord k, Ord a) => MinHeap k a -> Maybe ((k, a), MinHeap k a)
 extractMin Leaf = Nothing
 extractMin (Node a k l r) = Just ((k, a), merge l r)
